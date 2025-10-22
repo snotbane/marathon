aeb9da5319569125fe1cb723b121e7c5123a9ded
snotbane <27900656+snotbane@users.noreply.github.com>
2025-10-22 09:07:14 -0500

    Fixup

--- util/Utils.gd
+++ util/Utils.gd
@@ -6,7 +6,7 @@ static func get_project_preferred_path(path: String) -> String:
 	project_root_global = project_root_global.substr(0, project_root_global.length() - 1)
 	var path_global := ProjectSettings.globalize_path(path)
 	var path_belongs_to_project := path_global.begins_with(project_root_global)
-	return ("res://" + path_global.substr(project_root_global.length())) if path_belongs_to_project else path_global
+	return ("res:/" + path_global.substr(project_root_global.length())) if path_belongs_to_project else path_global
 
 
 static func print_ancestry(node: Node) -> void:
